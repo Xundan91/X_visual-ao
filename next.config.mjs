a7085f5153c29aa6ccd230a6bc67b3a625abb50d
ankushKun <ankush4singh@gmail.com>
2024-12-16 20:44:16 +0530

    basepath

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  transpilePackages: ["react-blockly"],
-  basePath: "/vao"
+  transpilePackages: ["react-blockly"]
 };
 
 export default nextConfig;
